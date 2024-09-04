0b84378496abc320890178486d3739522ea24cf6
JujuCode <60157186+JujuCode@users.noreply.github.com>
2024-09-04 17:33:48 -0400

    Po mal daffaire
    OUi jven po mal dchose, po gratui, mé presqe

--- dnn_framework/student/layers.py
+++ dnn_framework/student/layers.py
@@ -9,16 +9,26 @@ class FullyConnectedLayer(Layer):
     """
 
     def __init__(self, input_count, output_count):
-        raise NotImplementedError()
+        # Input and output count
+        self.input_count = input_count
+        self.output_count = output_count
+        # Parameters w and
+        self.parameters = {
+            'w': np.zeros(shape=(self.output_count, self.input_count)),
+            'b': np.zeros(shape=(1, self.output_count))
+        }
+        # raise NotImplementedError()
 
     def get_parameters(self):
-        raise NotImplementedError()
+        return self.parameters
 
     def get_buffers(self):
         raise NotImplementedError()
 
     def forward(self, x):
-        raise NotImplementedError()
+        w = self.get_parameters().get('w')
+        b = self.get_parameters().get('b')
+        return ((x @ np.transpose(w)) + b), None
 
     def backward(self, output_grad, cache):
         raise NotImplementedError()
@@ -30,19 +40,57 @@ class BatchNormalization(Layer):
     """
 
     def __init__(self, input_count, alpha=0.1):
-        raise NotImplementedError()
+
+        self.input_count = input_count
+        self.alpha = alpha
+        self.epsilon = 1e-10
+
+        # Parameters to be learned
+        #self.gamma = np.ones(shape=(1, input_count))
+        #self.beta = np.zeros(shape=(1, input_count))
+        self.parameters = {
+            'gamma': np.zeros(shape=(1, self.input_count)),
+            'beta': np.zeros(shape=(1, self.input_count))
+        }
+
+        # Buffers
+        self.running_mean = np.zeros(input_count)
+        self.running_var = np.ones(input_count)
+
+        # Flag for training / evaluation mode
+        self.training = True
 
     def get_parameters(self):
-        raise NotImplementedError()
+        return self.parameters
 
     def get_buffers(self):
-        raise NotImplementedError()
+        return self.running_mean, self.running_var
 
     def forward(self, x):
-        raise NotImplementedError()
+        if self.training:
+            return self._forward_training(x)
+        else:
+            return self._forward_evaluation(x)
 
     def _forward_training(self, x):
-        raise NotImplementedError()
+
+        # Calcul de la moyenne et de la variance
+        self.mean = np.mean(x, axis=0)
+        self.variance = np.var(x, axis=0)
+
+        # Normalisation de l'entrée
+        self.x_hat = (x - self.mean) / np.sqrt(self.variance + self.epsilon)
+        self.y = self.get_parameters().get('gamma') * self.x_hat + self.get_parameters().get('beta')
+
+        # Mise à jour des statistiques de l'ensemble du lot
+        self.running_mean = ((1 - self.alpha) * self.running_mean) + self.alpha * self.mean
+        self.running_var = ((1 - self.alpha) * self.running_var) + self.alpha * self.variance
+
+        # Place dans le cache x, x_hat, la moyenne et la variance
+        self.cache = (x, self.x_hat, self.mean, self.variance)
+
+        # Retourne la sortie
+        return self.y, None
 
     def _forward_evaluation(self, x):
         raise NotImplementedError()
@@ -63,10 +111,14 @@ class Sigmoid(Layer):
         raise NotImplementedError()
 
     def forward(self, x):
-        raise NotImplementedError()
+        y = 1/(1 + np.exp(-x))
+        return y, x
 
     def backward(self, output_grad, cache):
-        raise NotImplementedError()
+        sigmoid_forward = 1/(1 + np.exp(-cache))
+        input_grad = sigmoid_forward * (1 - sigmoid_forward)
+        y = output_grad * input_grad
+        return y, None
 
 
 class ReLU(Layer):
@@ -81,7 +133,7 @@ class ReLU(Layer):
         raise NotImplementedError()
 
     def forward(self, x):
-        raise NotImplementedError()
+        return np.maximum(x, 0), x
 
     def backward(self, output_grad, cache):
-        raise NotImplementedError()
+        return output_grad * (cache > 0), None
